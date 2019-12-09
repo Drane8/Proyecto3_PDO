48a5e1976d1f97f407a2d21b309cbcb4bb79539a
Dani <danigb92@hotmail.com>
2019-12-09 12:49:48 +0100

    Creado formulario inicial y empezado mostrar resultado

--- controladores/Controlador.php
+++ controladores/Controlador.php
@@ -3,7 +3,35 @@ class Controlador
 {
     public function run()
     {
-        
+        if (!isset($_POST['insertar']))//no se ha enviado el formulario
+        { // primera petición
+            //se llama al método para mostrar el formulario inicial
+            $this->mostrarFormulario();
+			exit();
+        } else
+        {
+            //el formulario ya se ha enviado
+            //se recogen y procesan los datos
+            //se llama al método para mostrar el resultado
+            $aula=$_POST['aula'];
+
+            //IMPORTANTE A HACER explode 
+            $articulo=$_POST['articulo'];
+            $cantidadArticulos = $_POST['cantidadArticulos'];
+            $resultado ="Bienvenido/a $aula" . $articulo;
+            $this->mostrarResultado($resultado);
+			exit();		
+        }
+    }
+    private function mostrarFormulario()
+    {
+     //se muestra la vista del formulario (la plantilla form_bienvenida.php)   
+        include 'vistas/form_inicial.php';
+    }
+    private function mostrarResultado($resultado)
+    {
+    // y se muestra la vista del resultado (la plantilla resultado.,php)
+        include 'vistas/vista_resultado.php';
     }
 }
 ?>
---
+++ vistas/form_inicial.php
@@ -0,0 +1,67 @@
+<?php
+include "cabecera.php";
+echo "<script>
+  document.getElementById('insertar').classList.add('active');
+</script>";
+$aulas = array('A04', 'A05', 'A06', 'A13', 'A17', 'C12');
+$articulos = array(
+    array(
+        'codigo' => '110',
+        'articulo' => 'ODENADOR PC'
+    ),
+    array(
+        'codigo' => '120',
+        'articulo' => 'ODENADOR SERVIDOR RED'
+    ),
+    array(
+        'codigo' => '130',
+        'articulo' => 'OTROS-ORDENADORES'
+    ),
+    array(
+        'codigo' => '210',
+        'articulo' => 'IMPRESORAS LASER'
+    ),
+    array(
+        'codigo' => '211',
+        'articulo' => 'IMPRESORAS LASER'
+    ),
+    array(
+        'codigo' => '212',
+        'articulo' => 'IMPRESORAS LASER'
+    )
+
+);
+?>
+<form id="form" action="index.php" method="post">
+    <div id="datos">
+        <select id="aula" name="aula">
+            <option value="" class="oculto">--SELECCIONE AULA--</option>
+            <?php
+            foreach ($aulas as $aula) {
+                echo "<option value='$aula'>$aula</option>";
+            }
+            ?>
+        </select>
+        <select id="articulo" name="articulo">
+            <option value="" class="oculto">--SELECCIONE ARTICULO--</option>
+            <?php
+            foreach ($articulos as $articulo) {
+                echo "<option value='".$articulo['codigo'] ." ".$articulo['articulo']."'>" . $articulo['articulo'] . "</option>";
+            }
+            ?>
+        </select>
+        <label for="cantidadArticulos">Cantidad de articulos: </label><br />
+        <input type="range" min="1" value="1" id="cantidadArticulos" name="cantidadArticulos" oninput="valorRango.value = this.value">
+        <output id="valorRango">1</output><br />
+        <label for="fecha">Fecha compra: </label>
+        <input type="date" id="fecha" name="fecha" /><br />
+        <label for="observaciones">Observaciones:</label><br />
+        <textarea id="observaciones" name="observaciones" rows="6" cols="40" maxlength="250"></textarea><br />
+        <div id="botones">
+            <input type="submit" name="insertar" value="Añadir" /><input type="reset" value="Limpiar" />
+        </div>
+    </div>
+</form>
+<?php
+include "pie.php";
+?>
